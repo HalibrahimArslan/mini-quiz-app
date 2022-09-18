0a194874089455f26d9d316ccab5774e1ef3c11f
Halil İbrahim Arslan <halilibrahim.arslan@aurorabilisim.com>
2022-09-18 14:07:33 +0300

    first-commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function control(formId){
+    console.log("Hello World")
+    console.log(document.getElementById(formId).querySelectorAll(':invalid'));
+}
+
+function changeControl(){
+    let name;
+    const formName = document.getElementById('name');
+
+    formName.addEventListener('change',() => {
+        
+    })
+     
+
+
+}
+
+function submitClick(){
+    let form = document.getElementById('loginForm');
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+        let formName = document.getElementById('name').value;
+        let formSurname = document.getElementById('surname').value;
+
+        localStorage.setItem('formName',formName);
+        localStorage.setItem('formSurname',formSurname);
+
+        console.log(formName,formSurname);
+        window.location.assign("/dashboard.html")
+       
+    })
+}
+
+changeControl();
+submitClick();
+
